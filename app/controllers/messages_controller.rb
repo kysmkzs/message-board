18901e0d8793647170473cc71114d2db02ed2f7f
kysmkzs1 <masa@mealthy.me>
2015-08-24 18:13:42 +0000

    edit add

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,5 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update]
   def index
     # Messageを全て取得する。
     @messages = Message.all
@@ -18,11 +19,28 @@ class MessagesController < ApplicationController
     end
   end
 
+  def edit
+  end
+  
+  def update
+    if @message.update(message_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+
   private
   def message_params
     # params[:message]のパラメータで name , bodyのみを許可する。
     # 返り値は ex:) {name: "入力されたname" , body: "入力されたbody" }
     params.require(:message).permit(:name, :body)
+  end
+
+  def set_message
+    @message = Message.find(params[:id])
   end
   ## ここまで
 end
